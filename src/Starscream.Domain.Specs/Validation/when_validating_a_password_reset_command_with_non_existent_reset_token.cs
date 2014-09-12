5aa2a2717e0614871b1685ad3e9c3377a8406ac8
Byron Sommardahl <byron@acklenavenue.com>
2014-09-12 17:52:10 -0500

    Added validator.

---
+++ src/Starscream.Domain.Specs/Validation/when_validating_a_password_reset_command_with_non_existent_reset_token.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using Moq;
+using Starscream.Domain.Application.Commands;
+using Starscream.Domain.Entities;
+using Starscream.Domain.Exceptions;
+using Starscream.Domain.Services;
+using Starscream.Domain.Validators;
+using Starscream.Domain.ValueObjects;
+using It = Machine.Specifications.It;
+
+namespace Starscream.Domain.Specs.Validation
+{
+    public class when_validating_a_password_reset_command_with_non_existent_reset_token
+    {
+        static ICommandValidator<ResetPassword> _validator;
+        static readonly EncryptedPassword EncryptedPassword = new EncryptedPassword("password");
+        static readonly Guid ResetPasswordToken = Guid.NewGuid();
+        static List<ValidationFailure> _expectedFailures;
+        static Exception _exception;
+        static IReadOnlyRepository _readOnlyRepo;
+
+        Establish context =
+            () =>
+            {
+                _readOnlyRepo = Mock.Of<IReadOnlyRepository>();
+                _validator = new PassowrdResetValidator(_readOnlyRepo);
+
+                _expectedFailures = new List<ValidationFailure>
+                                    {
+                                        new ValidationFailure(
+                                            "ResetPasswordToken",
+                                            ValidationFailureType.DoesNotExist)
+                                    };
+
+                Mock.Get(_readOnlyRepo).Setup(x => x.GetById<PasswordResetToken>(ResetPasswordToken))
+                    .Throws(new ItemNotFoundException<PasswordResetToken>(ResetPasswordToken));
+            };
+
+        Because of =
+            () => _exception = Catch.Exception(() =>
+                _validator.Validate(new VisitorSession(),
+                    new ResetPassword(ResetPasswordToken, EncryptedPassword)));
+
+        It should_return_expected_failures =
+            () => ((CommandValidationException) _exception).ValidationFailures.ShouldBeLike(_expectedFailures);
+    }
+}
---
+++ src/Starscream.Domain/Validators/PassowrdResetValidator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Starscream.Domain.Application.Commands;
+using Starscream.Domain.Entities;
+using Starscream.Domain.Exceptions;
+using Starscream.Domain.Services;
+
+namespace Starscream.Domain.Validators
+{
+    public class PassowrdResetValidator : ICommandValidator<ResetPassword>
+    {
+        readonly IReadOnlyRepository _readOnlyRepo;
+
+        public PassowrdResetValidator(IReadOnlyRepository readOnlyRepo)
+        {
+            _readOnlyRepo = readOnlyRepo;
+        }
+
+        public void Validate(IUserSession userSession, ResetPassword command)
+        {
+            var failures = new List<ValidationFailure>();
+            if (command.EncryptedPassword==null || string.IsNullOrEmpty(command.EncryptedPassword.Password))
+            {
+                failures.Add(new ValidationFailure("EncryptedPassword", ValidationFailureType.Missing));
+            }
+            if (command.ResetPasswordToken == Guid.Empty)
+            {
+                failures.Add(new ValidationFailure("ResetPasswordToken", ValidationFailureType.Missing));
+            }
+            else
+            {
+                try
+                {
+                    _readOnlyRepo.GetById<PasswordResetToken>(command.ResetPasswordToken);
+                }
+                catch (ItemNotFoundException<PasswordResetToken>)
+                {
+                    failures.Add(new ValidationFailure("ResetPasswordToken", ValidationFailureType.DoesNotExist));
+                }
+            }
+            if (failures.Any())
+                throw new CommandValidationException(failures);
+        }
+    }
+}
