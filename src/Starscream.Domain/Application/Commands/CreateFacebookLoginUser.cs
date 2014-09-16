d686caeaa7b74c87df98823af3bd134cf7c5df42
dantedubon <dante.dubon@gmail.com>
2014-09-16 10:28:01 -0500

    added Create Facebok User command

---
+++ src/Starscream.Domain/Application/Commands/CreateFacebookLoginUser.cs
@@ -0,0 +1,10 @@
+namespace Starscream.Domain.Application.Commands
+{
+    public class CreateFacebookLoginUser
+    {
+        public CreateFacebookLoginUser(string id, string email, string firstName, string lastName, string link, string name)
+        {
+                                            
+        }
+    }
+}
