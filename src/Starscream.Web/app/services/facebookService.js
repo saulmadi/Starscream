e2c7d5806255ead6ae72f8283b5af39fb2c442db
Javier Suazo <javier@acklenavenue.com>
2014-09-16 15:58:09 -0600

    Finished integration facebook

--- src/Starscream.Web/app/services/facebookService.js
+++ src/Starscream.Web/app/services/facebookService.js
@@ -1,15 +1,32 @@
-﻿angular.module('Starscream.Services').factory('facebookService', function ($q, $httpq, accountService) {
+﻿angular.module('Starscream.Services').factory('facebookService', function ($q, $httpq, loginService, accountService) {
 
     return {
-        Register: function () {
+        Register: function() {
+            var def = $q.defer();
+            FB.login(function(response) {
+                if (response.authResponse) {
+                    FB.api('/me', function(response) {
+                        accountService.RegisterFacebook(response).then(function() {
+                            def.resolve();
+                        }).catch(function() {
+                            def.reject();
+                        });
+                    });
+                } else {
+                    def.reject();
+                }
+            });
+            return def.promise;
+        },
+        Login: function () {
             var def = $q.defer();
             FB.login(function (response) {
                 if (response.authResponse) {
                     FB.api('/me', function (response) {
-                        accountService.RegisterFacebook(response).then(function () {
-                            def.resolve();
-                        }).catch(function () {
-                            def.reject();
+                        loginService.LoginFacebook({Id : response.id, Email: response.email}).then(function (data) {
+                            def.resolve(data);
+                        }).catch(function (error) {
+                            def.reject(error);
                         });
                     });
                 } else {
