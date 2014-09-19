2a2d82f2ab65abbda880cd554a2311aa92804641
Javier Suazo <javier@acklenavenue.com>
2014-09-19 09:50:39 -0600

    adding disable user exception

---
+++ src/Starscream.Domain/Exceptions/DisableUserAccountException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Starscream.Domain.Exceptions
+{
+    public class DisableUserAccountException : Exception
+    {
+         public DisableUserAccountException()
+             : base("Your account has been disabled. Please contact your administrator for help.")
+        {
+        }
+    }
+}
