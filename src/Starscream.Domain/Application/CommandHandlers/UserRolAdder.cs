31b0236831ab71ccd5924f641a734aa644666636
dantedubon <dante.dubon@gmail.com>
2014-09-27 12:30:01 -0500

    added command handler to manage adding rol to user

--- src/Starscream.Domain/Application/CommandHandlers/UserRolAdder.cs
+++ src/Starscream.Domain/Application/CommandHandlers/UserRolAdder.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using AcklenAvenue.Commands;
+using Starscream.Domain.Application.Commands;
 using Starscream.Domain.DomainEvents;
+using Starscream.Domain.Entities;
 using Starscream.Domain.Services;
 
 namespace Starscream.Domain.Application.CommandHandlers
@@ -20,7 +22,16 @@ namespace Starscream.Domain.Application.CommandHandlers
 
         public void Handle(IUserSession userIssuingCommand, AddRoleToUser command)
         {
-            throw new NotImplementedException();
+
+            var user = _readOnlyRepository.GetById<User>(command.UserId);
+            var role = _readOnlyRepository.GetById<Role>(command.RolId);
+
+            user.AddRol(role);
+
+            _writeableRepository.Update(user);
+            NotifyObservers(new UserRoleAdded(user.Id, role.Id));
+
+
         }
 
         public event DomainEvent NotifyObservers;
--- src/Starscream.Domain/Application/Commands/AddRoleToUser.cs
+++ src/Starscream.Domain/Application/Commands/AddRoleToUser.cs
@@ -2,12 +2,12 @@ using System;
 
 namespace Starscream.Domain.Application.Commands
 {
-    public class UserRoleAdded 
+    public class AddRoleToUser 
     {
         public Guid UserId { get; protected set; }
         public Guid RolId { get; protected set; }
 
-        public UserRoleAdded(Guid userId, Guid rolId)
+        public AddRoleToUser(Guid userId, Guid rolId)
         {
             UserId = userId;
             RolId = rolId;
