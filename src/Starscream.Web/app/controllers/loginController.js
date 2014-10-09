37cd036d9c817397f25b24a30902d87e396a0587
dantedubon <dante.dubon@gmail.com>
2014-10-09 11:04:04 -0500

    menu working

--- src/Starscream.Web/app/controllers/loginController.js
+++ src/Starscream.Web/app/controllers/loginController.js
@@ -1,4 +1,4 @@
-﻿angular.module('Starscream.Controllers').controller('loginController', function ($scope, $location, accountService, loginService, userService, facebookService, googleService) {
+﻿angular.module('Starscream.Controllers').controller('loginController', function ($scope, $location, accountService, loginService, userService, facebookService, googleService, menuService) {
         
     if (userService.GetUser()) {
         $location.path("/home");
@@ -6,11 +6,12 @@
 
     $scope.user = {};
     $scope.$parent.title = "Login";
-
+    
 
     var setUserSession = function(data) {
         userService.SetUser($scope.user.email, data.name, data.token, $scope.rememberMe, data.expires,data.claims);
         $scope.$parent.user = userService.GetUser();
+        $scope.$parent.menu = menuService.getMenuForUser($scope.$parent.user.claims || []);
         $location.path("/home");
     };
     
